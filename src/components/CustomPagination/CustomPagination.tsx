0ca41bec77fe9b80eec2a7f523507e78eb060242
dominik.bartoszcze <dominik.bartoszcze@britenet.com.pl>
2021-08-26 22:05:17 +0200

    feat/SA-7 create single product view for list

--- src/components/CustomPagination/CustomPagination.tsx
+++ src/components/CustomPagination/CustomPagination.tsx
@@ -1,46 +1,46 @@
 import React from 'react';
-import { Pagination } from 'antd';
+import {Pagination} from 'antd';
 import styles from './CustomPagination-styles.module.less';
 
 interface IPaginationView {
-  onChange: (page: number, e?: number | undefined) => void;
-  onShowSizeChange: (current: number, size: number) => Promise<void>;
-  total: number;
-  currentPage: number;
-  showSizeChanger?: boolean;
-  pageSizeOptions?: string[];
-  hideOnSinglePage?: boolean;
-  defaultPageSize?: number;
+    onChange: (page: number, e?: number | undefined) => void;
+    onShowSizeChange: (current: number, size: number) => Promise<void>;
+    total: number;
+    currentPage: number;
+    showSizeChanger?: boolean;
+    pageSizeOptions?: string[];
+    hideOnSinglePage?: boolean;
+    defaultPageSize?: number;
 }
 
 const PaginationView = ({
-  onChange,
-  onShowSizeChange,
-  total,
-  currentPage,
-  showSizeChanger = true,
-  defaultPageSize = 10,
-  pageSizeOptions = ['10', '20', '50'],
-  hideOnSinglePage,
-}: IPaginationView) => {
-  return (
-    <div className={styles.pagination}>
-      {total ? (
-        <Pagination
-          onChange={onChange}
-          showSizeChanger={showSizeChanger}
-          defaultCurrent={1}
-          total={total}
-          onShowSizeChange={onShowSizeChange}
-          current={currentPage}
-          defaultPageSize={defaultPageSize}
-          pageSizeOptions={pageSizeOptions}
-          hideOnSinglePage={hideOnSinglePage}
-          locale={'pl_PL'}
-        />
-      ) : null}
-    </div>
-  );
+                            onChange,
+                            onShowSizeChange,
+                            total,
+                            currentPage,
+                            showSizeChanger = true,
+                            defaultPageSize = 12,
+                            pageSizeOptions = ['12', '24', '36', '48'],
+                            hideOnSinglePage,
+                        }: IPaginationView) => {
+    return (
+        <div className={styles.pagination}>
+            {total ? (
+                <Pagination
+                    onChange={onChange}
+                    showSizeChanger={showSizeChanger}
+                    defaultCurrent={1}
+                    total={total}
+                    onShowSizeChange={onShowSizeChange}
+                    current={currentPage}
+                    defaultPageSize={defaultPageSize}
+                    pageSizeOptions={pageSizeOptions}
+                    hideOnSinglePage={hideOnSinglePage}
+                    locale={'pl_PL'}
+                />
+            ) : null}
+        </div>
+    );
 };
 
 
